812261ae7406f3884f436baed2340c878f796892
wvwall <waltervelardo@gmail.com>
2021-03-10 14:30:30 +0100

    Hamburger menu down & up completo

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 js-jq-hamburger
+mostrare / nascondere il menu principale (ricordate che per vedere l’hamburger in alto a destra dovete scendere a risoluzioni tipo tablet o mobile).
---
+++ main.js
@@ -0,0 +1,6 @@
+var menugiu = $(".fa-bars").click(function(){
+    $(".hamburger-menu").show();
+});
+var menusu = $(".fa-times").click(function(){
+    $(".hamburger-menu").hide();
+});
